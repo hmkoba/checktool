cdcd25fa0679c2c5f44d16c15a20bb9b608fd29c
kobayashi-hid <kobayashi-hid@gnavi.co.jp>
2018-07-23 00:00:09 +0900

    [master]

--- main.go
+++ main.go
@@ -8,41 +8,79 @@ import (
   "log"
 )
 
-type scrapingItem struct {
-  Selector string `json:"selector"`
-  Attr string `json:"attr"`
+type scrapingSetting struct {
+  NextPage struct {
+    Selector string `json:"selector"`
+    Attr string `json:"attr"`
+  } `json:"next_page"`
+
+  ScrapingItems []struct {
+    Name string `json:"name"`
+    Items []struct {
+      Selector string `json:"selector"`
+      Attr string `json:"attr"`
+      IsParent bool `json:"is_parent"`
+
+      Items []struct {
+        Selector string `json:"selector"`
+        Attr string `json:"attr"`
+        IsParent bool `json:"is_parent"`
+      } `json:"items"`
+    } `json:"items"`
+  } `json:"scraping_items"`
 }
 
 func main() {
 
-  items, err := read_setting()
+  scrapingSetting, err := read_setting()
   if err != nil {
     return
   }
-  for _, item := range items {
-    fmt.Printf("%#v", item)
-  }
-  scraping_url("", items)
+  fmt.Printf("%+v", scrapingSetting)
+
+  scraping_url("", scrapingSetting)
 
 }
-func read_setting() ([]scrapingItem, error) {
+
+func read_setting() (scrapingSetting, error) {
   file_path := "./setting.json"
   // JSONファイル読み込み
   bytes, err := ioutil.ReadFile(file_path)
   if err != nil {
+    fmt.Print("JSON read error:")
     log.Fatal(err)
   }
   // JSONデコード
-  var items []scrapingItem
-  err = json.Unmarshal(bytes, &items)
+  var setting scrapingSetting
+  err = json.Unmarshal(bytes, &setting)
   if err != nil {
+    fmt.Print("JSON decode error:")
     log.Fatal(err)
   }
 
-  return items, err
+  return setting, err
 }
 
-func scraping_url(url string, items []scrapingItem) {
+func format_line(line string, add string) string {
+  if line == "" {
+    return add
+  }
+  return line + "\t" + add
+}
+
+func get_attr(s *goquery.Selection, attr string) string {
+  if attr == "text" {
+    return s.Text()
+  } else {
+    ret, exists := s.First().Attr(attr)
+    if exists {
+      return ret
+    }
+    return ""
+  }
+}
+
+func scraping_url(url string, setting scrapingSetting) {
 
   doc, err := goquery.NewDocument(url)
   if err != nil {
@@ -50,20 +88,45 @@ func scraping_url(url string, items []scrapingItem) {
       return
   }
 
-  hasNext, exists := true, true
+  hasNext := true
   for hasNext {
     fmt.Println(url)
     fmt.Println("---------------------------------")
-    
-    doc.Find("h2.p-result__name").Each(func(_ int, s *goquery.Selection) {
-          fmt.Println(s.Text())
-    })
-    url, exists = doc.Find("link[rel^='next']").First().Attr("href")
-    if exists {
-      doc, err = goquery.NewDocument(url)
-      if err != nil {
-          log.Fatal(err)
-          return
+
+    for _, scrapingItem := range setting.ScrapingItems {
+      fmt.Println(scrapingItem.Name)
+
+      for _, item := range scrapingItem.Items {
+        doc.Find(item.Selector).Each(func(_ int, s *goquery.Selection) {
+          result_line := ""
+
+          if item.Attr != "" {
+            result_line = format_line(result_line, get_attr(s, item.Attr))
+          }
+
+          for _, child_item := range item.Items {
+            s.Find(child_item.Selector).Each(func(_ int, cs *goquery.Selection) {
+              result_line = format_line(result_line, get_attr(cs, child_item.Attr))
+            })
+          }
+          result_line += "\n"
+          fmt.Print(result_line)
+        })
+      }
+    }
+
+    if setting.NextPage.Selector != "" {
+
+      url, exists := doc.Find(setting.NextPage.Selector).First().Attr(setting.NextPage.Attr)
+
+      if ! exists || url == "" {
+        hasNext = false
+      } else {
+        doc, err = goquery.NewDocument(url)
+        if err != nil {
+            log.Fatal(err)
+            return
+        }
       }
     }
   }
